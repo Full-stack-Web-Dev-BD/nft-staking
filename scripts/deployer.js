6cff2317d8b824f6d6ed53c5c14cee19cc24d989
Alamin <alaminprogramerr@gmail.com>
2022-04-22 16:08:31 +0600

    nft contract

--- scripts/deployer.js
+++ scripts/deployer.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const MetaShoesNFTStaking = await hre.ethers.getContractFactory("MetaShoesNFTStaking");
+  const metaShoesNFTStaking = await MetaShoesNFTStaking.deploy();
 
-  await greeter.deployed();
+  await metaShoesNFTStaking.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("MetaShoesNFTStaking deployed to:", metaShoesNFTStaking.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
